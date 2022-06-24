7e94caee21d965b64bded21fdd5200d0d7a599ca
Celina <wittmarcel13@gmail.com>
2022-06-24 21:40:47 +0200

    Sending messages Async to Discord, to prevent Minecraft-Chat lagging behind.

--- src/main/java/com/github/cxlina/smpbot/discord/SpigotListener.java
+++ src/main/java/com/github/cxlina/smpbot/discord/SpigotListener.java
@@ -6,6 +6,7 @@ import com.github.cxlina.smpbot.util.TranslationUtil;
 import de.jeff_media.jefflib.data.AdvancementInfo;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Member;
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -22,28 +23,36 @@ public class SpigotListener implements Listener {
     @EventHandler(priority = EventPriority.MONITOR)
     public void onMinecraftChatMessage(AsyncPlayerChatEvent e) {
         Member m = ConfigUtil.getDiscordMember(e.getPlayer());
-        Main.getPlugin().getBot().getJDA().getGuildById(ConfigUtil.getMainGuildID()).getTextChannelById(ConfigUtil.getChatChannelID()).sendMessage("**" + (m != null ? m.getEffectiveName() : e.getPlayer().getName()) + "**: " + e.getMessage()).queue();
+        Bukkit.getScheduler().runTaskAsynchronously(Main.getPlugin(), () -> {
+            Main.getPlugin().getBot().getJDA().getGuildById(ConfigUtil.getMainGuildID()).getTextChannelById(ConfigUtil.getChatChannelID()).sendMessage("**" + (m != null ? m.getEffectiveName() : e.getPlayer().getName()) + "**: " + e.getMessage()).queue();
+        });
     }
 
     @EventHandler
     public void onMinecraftJoin(PlayerJoinEvent e) {
         Member m = ConfigUtil.getDiscordMember(e.getPlayer());
         EmbedBuilder b = new EmbedBuilder().setColor(Color.GREEN).setTitle("Simp-SMP").setDescription((m == null ? e.getPlayer().getName() : (m.getRoles().isEmpty() ? "" : "[" + m.getRoles().get(0).getName() + "] ") + m.getEffectiveName()) + " joined the Server.");
-        Main.getPlugin().getBot().getJDA().getGuildById(ConfigUtil.getMainGuildID()).getTextChannelById(ConfigUtil.getChatChannelID()).sendMessageEmbeds(b.build()).queue();
+        Bukkit.getScheduler().runTaskAsynchronously(Main.getPlugin(), () -> {
+            Main.getPlugin().getBot().getJDA().getGuildById(ConfigUtil.getMainGuildID()).getTextChannelById(ConfigUtil.getChatChannelID()).sendMessageEmbeds(b.build()).queue();
+        });
     }
 
     @EventHandler
     public void onMinecraftQuit(PlayerQuitEvent e) {
         Member m = ConfigUtil.getDiscordMember(e.getPlayer());
         EmbedBuilder b = new EmbedBuilder().setColor(Color.RED).setTitle("Simp-SMP").setDescription((m == null ? e.getPlayer().getName() : (m.getRoles().isEmpty() ? "" : "[" + m.getRoles().get(0).getName() + "] ") + m.getEffectiveName()) + " left the Server.");
-        Main.getPlugin().getBot().getJDA().getGuildById(ConfigUtil.getMainGuildID()).getTextChannelById(ConfigUtil.getChatChannelID()).sendMessageEmbeds(b.build()).queue();
+        Bukkit.getScheduler().runTaskAsynchronously(Main.getPlugin(), () -> {
+            Main.getPlugin().getBot().getJDA().getGuildById(ConfigUtil.getMainGuildID()).getTextChannelById(ConfigUtil.getChatChannelID()).sendMessageEmbeds(b.build()).queue();
+        });
     }
 
     @EventHandler
     public void onMinecraftPlayerDeath(PlayerDeathEvent e) {
         Member m = ConfigUtil.getDiscordMember(e.getEntity());
         EmbedBuilder b = new EmbedBuilder().setColor(Color.GREEN).setTitle("Simp-SMP").setDescription((m == null ? e.getEntity().getName() : (m.getRoles().isEmpty() ? "" : "[" + m.getRoles().get(0).getName() + "] ") + m.getEffectiveName()) + " died. Reason: " + TranslationUtil.translateDamageReason(e.getEntity().getLastDamageCause().getCause().name()) + ".");
-        Main.getPlugin().getBot().getJDA().getGuildById(ConfigUtil.getMainGuildID()).getTextChannelById(ConfigUtil.getChatChannelID()).sendMessageEmbeds(b.build()).queue();
+        Bukkit.getScheduler().runTaskAsynchronously(Main.getPlugin(), () -> {
+            Main.getPlugin().getBot().getJDA().getGuildById(ConfigUtil.getMainGuildID()).getTextChannelById(ConfigUtil.getChatChannelID()).sendMessageEmbeds(b.build()).queue();
+        });
     }
 
     @EventHandler
@@ -56,6 +65,8 @@ public class SpigotListener implements Listener {
         if (e.getAdvancement().getKey().getKey().contains("recipe/")) return;
         Member m = ConfigUtil.getDiscordMember(e.getPlayer());
         EmbedBuilder b = new EmbedBuilder().setColor(Color.GREEN).setTitle("Simp-SMP").setDescription((m == null ? e.getPlayer().getName() : (m.getRoles().isEmpty() ? "" : "[" + m.getRoles().get(0).getName() + "] ") + m.getEffectiveName()) + " completed the Advancement " + info.getTitle() + ".");
-        Main.getPlugin().getBot().getJDA().getGuildById(ConfigUtil.getMainGuildID()).getTextChannelById(ConfigUtil.getChatChannelID()).sendMessageEmbeds(b.build()).queue();
+        Bukkit.getScheduler().runTaskAsynchronously(Main.getPlugin(), () -> {
+            Main.getPlugin().getBot().getJDA().getGuildById(ConfigUtil.getMainGuildID()).getTextChannelById(ConfigUtil.getChatChannelID()).sendMessageEmbeds(b.build()).queue();
+        });
     }
 }
