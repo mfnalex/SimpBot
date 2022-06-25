d648b2ab63da1753e1c81f455af291226c10bf51
mfnalex <1122571+mfnalex@users.noreply.github.com>
2022-06-25 04:07:57 +0200

    .

---
+++ src/main/java/com/github/cxlina/smpbot/util/HotEGirls.java
@@ -0,0 +1,21 @@
+package com.github.cxlina.smpbot.util;
+
+public class HotEGirls {
+
+    public static class InYourArea {
+
+    }
+
+    public static class OnDiscord {
+
+
+        public static class WantTo {
+
+            // TODO: method for embeds, avoid rewriting it all the time
+            public static void sendAnEmbed(String description) {
+                throw new UnsupportedOperationException();
+            }
+
+        }
+    }
+}
